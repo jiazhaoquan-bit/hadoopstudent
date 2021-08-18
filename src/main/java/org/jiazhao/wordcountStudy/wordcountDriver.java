84ac1139aaeb619f1aae81404fe0b4ca184e41d5
jiazhaoquan <jzq17856170605@163.com>
2021-08-18 16:01:51 +0800

    第一次測試

--- src/main/java/org/jiazhao/wordcountStudy/wordcountDriver.java
+++ src/main/java/org/jiazhao/wordcountStudy/wordcountDriver.java
@@ -10,25 +10,7 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
 
-//        Configuration conf = new Configuration();
-//        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-//        if (otherArgs.length < 2) {
-//            System.err.println("Usage: wordcount <in> [<in>...] <out>");
-//            System.exit(2);
-//        }
-//        Job job = Job.getInstance(conf, "word count");
-//        job.setJarByClass(wordcount.class);
-//        job.setMapperClass(TokenizerMapper.class);
-//        job.setCombinerClass(IntSumReducer.class);
-//        job.setReducerClass(IntSumReducer.class);
-//        job.setOutputKeyClass(Text.class);
-//        job.setOutputValueClass(IntWritable.class);
-//        for (int i = 0; i < otherArgs.length - 1; ++i) {
-//            FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
-//        }
-//        FileOutputFormat.setOutputPath(job,
-//                new Path(otherArgs[otherArgs.length - 1]));
-//        System.exit(job.waitForCompletion(true) ? 0 : 1);
+
 public class wordcountDriver {
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         //获取job
