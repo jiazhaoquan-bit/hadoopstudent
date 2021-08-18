94cece9f24a877a3367dfbec2110173a5ec28f4b
jiazhaoquan <jzq17856170605@163.com>
2021-08-18 16:08:01 +0800

    第一次測試

--- src/main/java/org/jiazhao/wordcountStudy/wordcountDriver.java
+++ src/main/java/org/jiazhao/wordcountStudy/wordcountDriver.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 
 public class wordcountDriver {
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-        //获取job
+
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf);
         //设置jar包路径
